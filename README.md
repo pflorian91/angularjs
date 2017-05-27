312c9feec8413437d9fec1808b8a77df712f547f
Florian Popa <florian@webgenerals.com>
2017-05-27 16:09:04 +0200

    Basic config.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # angularjs
 AngularJS
+
+Commands:
+> npm start
+> npm run webpack-prod
+> npm run build
+
+Based on https://github.com/vsternbach/angularjs-typescript-webpack
---
+++ src/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from 'angular-ts-decorators';
+
+@Component({
+	selector: 'app',
+	template: 'Hi there'
+	// template: `<comment-list></comment-list>`
+})
+
+export class AppComponent {
+}
